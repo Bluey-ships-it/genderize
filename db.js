f39e0cccbcca4a9231c15c7766cd2126e826b153
Bluey <wannaguessnow@gmail.com>
2026-04-17 13:07:43 +0100

    fix

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || process.env.DATABASE_PUBLIC_URL,
+	connectionString: process.env.DATABASE_URL || process.env.DATABASE_PUBLIC_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
+
 module.exports = pool;
